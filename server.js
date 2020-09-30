dea8bf58c8e9438cc388b0901ff4bfe31689784f
Avinash.B <avinash.fullstack@outlook.com>
2020-09-30 12:37:40 +0530

    app config

---
+++ server.js
@@ -0,0 +1,34 @@
+// importing
+import express from 'express'
+import mongoose from 'mongoose'
+
+
+// App config
+const app = express();
+const port = process.env.Port || 8001
+const connection_url = 'mongodb+srv://admin:avinash@cluster0.nj8ss.mongodb.net/tinderdb?retryWrites=true&w=majority'
+
+
+
+
+// Middleware
+
+
+
+// Db config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+
+})
+
+// Api Endpoints
+app.get('/', (req, res) => {
+    res.status(200).send("hello world")
+})
+
+
+// listener
+
+app.listen(port, () => console.log(`listening on localhost :${port}`))
